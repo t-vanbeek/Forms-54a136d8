bc698998cff773ddbf1d69ea982cd4f468fe7861
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-12-03 12:54:45 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <title>form.php</title>
+    </head>
+    <body>
+        <form>
+            <input type="email" name="email" id="mail" placeholder="Voer je e-mailadres in">
+            <input type="submit">
+        </form>
+    </body>
+</html>
